09c80f2b6a590e892a62a39b4d018a4cdfd5a281
Andrew North <anwnorth@gmail.com>
2022-07-31 13:28:00 -0400

    Add pascales Triangle to pythonfile.py

--- pythonfile.py
+++ pythonfile.py
@@ -16,3 +16,11 @@ def something(number):
     return number + int(random)
 
 print(something(0))
+
+print("1")
+print("1 1")
+print("1 2 1")
+print("1 3 3 1")
+print("1 4 6 4 1")
+print("1 5 10 10 5 1")
+print("1 6 15 20 15 6 1")
